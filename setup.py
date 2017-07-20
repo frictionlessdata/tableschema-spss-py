98c7e18e022f596a5f67dc061ed3013474f37143
Brook Elgie <specialbrew@gmail.com>
2017-07-20 16:08:57 +0100

    Standard lib structure

---
+++ setup.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import io
+import os.path
+from setuptools import setup, find_packages
+
+
+# Helpers
+def read(*segments):
+    path = os.path.join(*segments)
+    with io.open(path, encoding='utf-8') as f:
+        return f.read().strip()
+
+
+# Prepare
+PACKAGE = 'tableschema_spss'
+NAME = PACKAGE.replace('_', '-')
+INSTALL_REQUIRES = [
+    'six>=1.9,<2.0'
+]
+TESTS_REQUIRE = [
+    'pylama',
+    'tox',
+]
+README = read('README.md')
+VERSION = read(PACKAGE, 'VERSION')
+PACKAGES = find_packages(exclude=['tests'])
+
+
+# Run
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=PACKAGES,
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'develop': TESTS_REQUIRE},
+    zip_safe=False,
+    long_description=README,
+    description='',
+    author='Open Knowledge International',
+    author_email='info@okfn.org',
+    url='https://github.com/frictionlessdata/tableschema-spss-py',
+    license='MIT',
+    keywords=['frictionless data', 'datapackage', 'tableschema', 'spss'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
